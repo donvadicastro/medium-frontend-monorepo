484069fd547c77d72c5ac3fb01fbae85fb123910
vadimb <vadim.barylo@intapp.com>
2023-03-04 17:58:10 +0000

    small redesign

--- packages/islands-arch-webpack/webpack.config.js
+++ packages/islands-arch-webpack/webpack.config.js
@@ -1,8 +1,20 @@
 const { composePlugins, withNx, withWeb } = require('@nrwl/webpack');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withWeb(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+
+  config.plugins.push(
+    new ModuleFederationPlugin({
+      name: 'menu',
+      filename: 'remoteEntry.js',
+      remotes: {
+        menu: 'menu@http://localhost:8083/remoteEntry.js',
+      },
+    })
+  );
+
   return config;
 });
